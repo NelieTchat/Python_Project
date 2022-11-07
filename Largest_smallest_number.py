d26e4e534a23cc1ac6a0f2a5030bd03f27cc9f5f
Nelie <85373420+NelieTchat@users.noreply.github.com>
2022-11-07 14:42:14 -0800

    Finding largest and smallest values
    Python Program that Prints the largest and smallest values in a list using the min and max built-in-function.

---
+++ Largest_smallest_number.py
@@ -0,0 +1,28 @@
+print('case1')
+list_numbers = [3, 4, 5, 6]
+for number in list_numbers:
+    if number == 0:
+        print("None")
+    else:
+        largest = max(list_numbers)
+        smallest = min(list_numbers)
+print(f"{largest}  {smallest}")
+print()
+
+print('case2')
+list_numbers= [-1, -2, -3, -4]
+for number in list_numbers:
+    if number >= 0:
+        print("None")
+    else:
+        largest = max(list_numbers)
+        smallest = min(list_numbers)
+print(f"{largest} {smallest}")
+
+
+
+
+
+
+
+

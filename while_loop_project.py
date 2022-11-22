bf39fb6f378839bc6509da5f406773d88c647d05
Nelie <85373420+NelieTchat@users.noreply.github.com>
2022-11-22 15:54:45 -0800

    Update while_loop_project.py

--- while_loop_project.py
+++ while_loop_project.py
@@ -1,22 +1,5 @@
 
 # Constructing while loops that gives the user to enter username and password with 5 attempts.
-# user = 'Mike'
-# pw = '1234'
-#
-# print('Please enter your username and password: ')
-# username = ""
-# password = ""
-# count = 0
-#
-# while username != user and password != pw and count < 5:
-#   username = input('Username: ')
-#   password = input('password: ')
-#   if username == user and password == pw:
-#     print('Welcome',username)
-#   else:
-#     print('Denied, Please try again')
-#     count = count + 1
-###############
 user = 'Mike'
 pwd = '1234'
 attempts = 0

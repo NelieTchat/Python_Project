2546c80321c1be2d1f9f165e5c4c92cd4410fa52
Nelie <85373420+NelieTchat@users.noreply.github.com>
2022-11-22 16:02:10 -0800

    OUTPUT
    CASE1
    username: kuin
    password: 1356
    incorrect, Try again! 
    username: Mike
    password: 1234
    you are in!
    
    CASE2
    username: mijy
    password: 3654
    incorrect, Try again! 
    username: ndiuye
    password: 8795
    incorrect, Try again! 
    username: nhs.,dlk
    password: 16555
    incorrect, Try again! 
    username: 
    password: 
    incorrect, Try again! 
    username: asdjf
    password: 5748
    Account blocked, too many attempts

---
+++ while_loop_project.py
@@ -0,0 +1,21 @@
+# Constructing while loops that gives the user to enter username and password with 5 attempts.
+
+user = 'Mike'
+pwd = '1234'
+
+
+attempts = 0
+while attempts < 6:
+    attempts += 1
+    username = input('username: ')
+    password = input('password: ')
+
+    if username == user and password == pwd:
+        print('you are in!')
+        break
+    elif username != user and password != pwd:
+        if attempts < 5:
+            print('incorrect, Try again! ')
+        else:
+            print('Account blocked, too many attempts')
+            break
